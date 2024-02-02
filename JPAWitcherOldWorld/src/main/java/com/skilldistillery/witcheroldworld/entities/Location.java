0c95e80373d9cf64fee6dabe3d8c49d27795aa42
Johnelle Walker <johnellewalker@gmail.com>
2024-02-02 14:04:40 -0700

    Monster & Location Entities

---
+++ JPAWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/entities/Location.java
@@ -0,0 +1,141 @@
+package com.skilldistillery.witcheroldworld.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Location {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	@Column(name = "image_url")
+	private String imageUrl;
+	@ManyToMany(mappedBy = "locations")
+    private List<Monster> monsters;
+	@OneToMany(mappedBy = "location")
+	private List<Player> players;
+
+	public Location() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Monster> getMonsters() {
+		return monsters;
+	}
+
+	public void setMonsters(List<Monster> monsters) {
+		this.monsters = monsters;
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+
+	public void addMonster(Monster monster) {
+		if(monsters == null) {
+			monsters = new ArrayList<>();
+		}
+		
+		if(!monsters.contains(monster)) {
+			monsters.add(monster);
+			monster.addLocation(this);
+		}
+	}
+	
+	public void removeMonster(Monster monster) {
+		if(monsters != null && monsters.contains(monster)) {
+			monsters.remove(monster);
+			monster.removeLocation(this);
+		}
+	}
+
+	public void addPlayer(Player player) {
+	    if (players == null) {
+	        players = new ArrayList<>();
+	    }
+	    if (!players.contains(player)) {
+	        players.add(player);
+	        player.setLocation(this);
+	    }
+	}
+
+	public void removePlayer(Player player) {
+	    if (players != null && players.contains(player)) {
+	        players.remove(player);
+	        if (player != null) {
+	            player.setLocation(null);
+	        }
+	    }
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Location ID: " + id + "|| Name: " + name + "|| Description: " + description
+				+ "|| Monsters At Location: " + monsters.size() + "|| Players At Location: " + players.size();
+	}
+
+}
---
+++ JPAWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/entities/Monster.java
@@ -0,0 +1,127 @@
+package com.skilldistillery.witcheroldworld.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Monster {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int health;
+	private int damage;
+	@Column(name = "experience_reward")
+	private int experienceReward;
+	@ManyToMany
+	@JoinTable(name = "monster_location", 
+	joinColumns = @JoinColumn(name = "monster_id"), 
+	inverseJoinColumns = @JoinColumn(name = "location_id")) 
+    private List<Location> locations;
+
+	public Monster() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public int getExperienceReward() {
+		return experienceReward;
+	}
+
+	public void setExperienceReward(int experienceReward) {
+		this.experienceReward = experienceReward;
+	}
+
+	
+	public List<Location> getLocations() {
+		return locations;
+	}
+
+	public void setLocations(List<Location> locations) {
+		this.locations = locations;
+	}
+
+	public void addLocation(Location location) {
+		if(locations == null) {
+			locations = new ArrayList<>();
+		}
+		
+		if(!locations.contains(location)) {
+			locations.add(location);
+			location.addMonster(this);
+		}
+	}
+	
+	public void removeLocation(Location location) {
+		if(locations != null && locations.contains(location)) {
+			locations.remove(location);
+			location.removeMonster(this);
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Monster other = (Monster) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Monster ID: " + id + "|| Name: " + name + "|| Health: " + health + "|| Damage: " + damage
+				+ "|| Experience Points: " + experienceReward;
+	}
+
+}
