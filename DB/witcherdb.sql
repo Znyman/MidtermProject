4b5e04906bad11400dab788ec4bedb659a58a532
Johnelle Walker <johnellewalker@gmail.com>
2024-02-02 10:39:23 -0700

    DB update after project approval

--- DB/witcherdb.sql
+++ DB/witcherdb.sql
@@ -16,35 +16,53 @@ CREATE SCHEMA IF NOT EXISTS `witcherdb` DEFAULT CHARACTER SET utf8 ;
 USE `witcherdb` ;
 
 -- -----------------------------------------------------
--- Table `location`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `location` ;
-
-CREATE TABLE IF NOT EXISTS `location` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `health` INT UNSIGNED NULL,
-  `enabled` TINYINT NULL,
-  `location_id` INT NULL,
+  `username` VARCHAR(200) NOT NULL,
+  `email` VARCHAR(200) NULL,
+  `password` VARCHAR(200) NOT NULL,
   `role` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`, `password`),
-  UNIQUE INDEX `email_UNIQUE` (`username` ASC),
-  INDEX `fk_user_location1_idx` (`location_id` ASC),
-  CONSTRAINT `fk_user_location1`
+  `enabled` TINYINT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `location` ;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NOT NULL,
+  `image_url` TEXT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `player`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `player` ;
+
+CREATE TABLE IF NOT EXISTS `player` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` TEXT NOT NULL,
+  `current_health` INT NOT NULL,
+  `max_health` INT NOT NULL,
+  `experience_level` INT NOT NULL,
+  `image_url` TEXT NOT NULL,
+  `location_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_player_location1_idx` (`location_id` ASC),
+  CONSTRAINT `fk_player_location1`
     FOREIGN KEY (`location_id`)
     REFERENCES `location` (`id`)
     ON DELETE NO ACTION
@@ -59,14 +77,16 @@ DROP TABLE IF EXISTS `weapon` ;
 
 CREATE TABLE IF NOT EXISTS `weapon` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
   `damage` INT UNSIGNED NOT NULL,
-  `user_id` INT NOT NULL,
+  `description` VARCHAR(2000) NOT NULL,
+  `tier` INT NOT NULL,
+  `player_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_weapon_user_idx` (`user_id` ASC),
-  CONSTRAINT `fk_weapon_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
+  INDEX `fk_weapon_player1_idx` (`player_id` ASC),
+  CONSTRAINT `fk_weapon_player1`
+    FOREIGN KEY (`player_id`)
+    REFERENCES `player` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -79,14 +99,16 @@ DROP TABLE IF EXISTS `armor` ;
 
 CREATE TABLE IF NOT EXISTS `armor` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
   `defense` INT UNSIGNED NOT NULL,
-  `user_id` INT NOT NULL,
+  `description` VARCHAR(2000) NOT NULL,
+  `tier` INT NOT NULL,
+  `player_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_armor_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_armor_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
+  INDEX `fk_armor_player1_idx` (`player_id` ASC),
+  CONSTRAINT `fk_armor_player1`
+    FOREIGN KEY (`player_id`)
+    REFERENCES `player` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -99,13 +121,31 @@ DROP TABLE IF EXISTS `monster` ;
 
 CREATE TABLE IF NOT EXISTS `monster` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
   `health` INT NOT NULL,
   `damage` INT UNSIGNED NOT NULL,
+  `experience_reward` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `monster_location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `monster_location` ;
+
+CREATE TABLE IF NOT EXISTS `monster_location` (
+  `monster_id` INT NOT NULL,
   `location_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_monster_location1_idx` (`location_id` ASC),
-  CONSTRAINT `fk_monster_location1`
+  PRIMARY KEY (`monster_id`, `location_id`),
+  INDEX `fk_monster_has_location_location1_idx` (`location_id` ASC),
+  INDEX `fk_monster_has_location_monster1_idx` (`monster_id` ASC),
+  CONSTRAINT `fk_monster_has_location_monster1`
+    FOREIGN KEY (`monster_id`)
+    REFERENCES `monster` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_monster_has_location_location1`
     FOREIGN KEY (`location_id`)
     REFERENCES `location` (`id`)
     ON DELETE NO ACTION
@@ -128,7 +168,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `witcherdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `health`, `enabled`, `location_id`, `role`) VALUES (1, 'walkerjohnelle', '9ol.)P:?', 100, 1, NULL, 'user');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `enabled`) VALUES (1, 'walkerjohnelle', NULL, '9ol.)P:?', 'user', 1);
 
 COMMIT;
 
