610eab4e1065068e138f3357b62131062cabc47a
DylanBeppler <dylanbeppler@gmail.com>
2024-02-03 13:15:43 -0600

    Created player dao and methods

--- BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/data/PlayerDAO.java
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/data/PlayerDAO.java
@@ -1,5 +1,16 @@
 package com.skilldistillery.witcheroldworld.data;
 
+import java.util.List;
+
+import com.skilldistillery.witcheroldworld.entities.Player;
+
 public interface PlayerDAO {
 
+	Player findById(int id);
+	Player createPlayer(Player player);
+	Player updatePlayer(Player player);
+	boolean deletePlayer(int id);
+	List<Player> findAll(int userId);
+	
+	
 }
---
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/data/PlayerDAOImpl.java
@@ -0,0 +1,60 @@
+package com.skilldistillery.witcheroldworld.data;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+import com.skilldistillery.witcheroldworld.entities.Player;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class PlayerDAOImpl implements PlayerDAO {
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public Player findById(int id) {
+		Player player = em.find(Player.class,id);
+		return player;
+	}
+
+	@Override
+	public Player createPlayer(Player player) {
+		em.persist(player);
+		return player;
+	}
+
+	@Override
+	public Player updatePlayer(Player player) {
+		Player managedPlayer = em.find(Player.class, player.getId());
+		managedPlayer.setName(player.getName());
+		managedPlayer.setDescription(player.getDescription());
+		managedPlayer.setMaxHealth(player.getMaxHealth());
+		managedPlayer.setExperienceLevel(player.getExperienceLevel());
+			
+		return managedPlayer;
+	}
+
+	@Override
+	public boolean deletePlayer(int id) {
+		Player managedPlayer = em.find(Player.class, id);
+		if(managedPlayer == null) {
+			return false;
+		}
+		em.remove(managedPlayer);
+		return true;
+	}
+
+	@Override
+	public List<Player> findAll(int userId) {
+		String jpql = "SELECT player FROM Player player WHERE user_id = :user_id";
+		List<Player> players = em.createQuery(jpql, Player.class).setParameter("user_id", userId).getResultList();
+		return players;
+	}
+
+}
