344890d93b21536b6ba23cc0d122bbc8a17a73aa
DylanBeppler <dylanbeppler@gmail.com>
2024-02-05 14:32:32 -0600

    updated the location controller annotation

--- BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/LocationController.java
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/LocationController.java
@@ -1,16 +1,15 @@
 package com.skilldistillery.witcheroldworld.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.skilldistillery.witcheroldworld.data.LocationDAO;
-import com.skilldistillery.witcheroldworld.entities.Location;
 import com.skilldistillery.witcheroldworld.entities.Player;
 
 import jakarta.servlet.http.HttpSession;
-
+@Controller
 public class LocationController {
 
     @Autowired
