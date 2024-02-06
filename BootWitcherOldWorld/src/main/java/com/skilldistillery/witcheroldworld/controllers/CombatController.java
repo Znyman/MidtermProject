05d7d8bade1924aeabc697026671154550a404f8
Zach Nyman <zachery.nyman@gmail.com>
2024-02-06 10:38:58 -0700

    Updated controllers

--- BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/CombatController.java
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/CombatController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.witcheroldworld.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.witcheroldworld.data.MonsterDAO;
 import com.skilldistillery.witcheroldworld.data.PlayerDAO;
@@ -15,6 +17,11 @@ public class CombatController {
 	@Autowired
 	private MonsterDAO monsterDAO;
 	
-	
+	@PostMapping("witcherAttack.do")
+	public String witcherAttack(@RequestParam("weaponId") int weaponId) {
+		
+		
+		return "showMonster";
+	}
 
 }
--- BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/MonsterController.java
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/MonsterController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.witcheroldworld.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,7 @@ import com.skilldistillery.witcheroldworld.data.MonsterDAO;
 import com.skilldistillery.witcheroldworld.entities.Location;
 import com.skilldistillery.witcheroldworld.entities.Monster;
 import com.skilldistillery.witcheroldworld.entities.Player;
+import com.skilldistillery.witcheroldworld.entities.Weapon;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -22,21 +25,13 @@ public class MonsterController {
 	public String showMonster(HttpSession session, Model model) {
 		Player currentPlayer = (Player) session.getAttribute("player");
 		Location currentLocation = currentPlayer.getLocation();
-		Monster monster;
-		switch (currentLocation.getName()) {
-		case "Kaer Seren":
-			monster = monsterDAO.findById(currentLocation.getId());
-		case "Hengfors":
-			monster = monsterDAO.findById(currentLocation.getId());
-		case "Cidaris":
-			monster = monsterDAO.findById(currentLocation.getId());
-		case "Cintra":
-			monster = monsterDAO.findById(currentLocation.getId());
-		case "Novigrad":
-			monster = monsterDAO.findById(currentLocation.getId());
-		default:
-			monster = monsterDAO.findById(currentLocation.getId());
-		}
+		List<Weapon> weapons = currentPlayer.getWeapons();
+		Monster monster = monsterDAO.findById(currentLocation.getId());
+		monster.setPlayer(currentPlayer);
+		monster = monsterDAO.createMonster(monster);
+		session.setAttribute("weapons", weapons);
+		model.addAttribute("weapons", weapons);
+		session.setAttribute("monster", monster);
 		model.addAttribute("monster", monster);
 		return "showMonster";
 	}
