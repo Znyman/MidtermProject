36af6c339a2008d7c97a9ee2d14abc1fc3efb16f
Zach Nyman <zachery.nyman@gmail.com>
2024-02-06 15:53:56 -0700

    changed add in armor and weapon

--- BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/MonsterController.java
+++ BootWitcherOldWorld/src/main/java/com/skilldistillery/witcheroldworld/controllers/MonsterController.java
@@ -26,12 +26,11 @@ public class MonsterController {
 	@GetMapping("showMonster.do")
 	public String showMonster(HttpSession session, Model model) {
 		Player currentPlayer = (Player) session.getAttribute("player");
+		System.out.println("*************************************" + currentPlayer.getWeapons() + "****************************************");
 		List<Weapon> weapons = currentPlayer.getWeapons();
 		List<Armor> armors = currentPlayer.getArmors();
 		session.setAttribute("armors", armors);
-		model.addAttribute("armors", armors);
 		session.setAttribute("weapons", weapons);
-		model.addAttribute("weapons", weapons);
 		
 		if (currentPlayer.getMonster() == null) {
 			Location currentLocation = currentPlayer.getLocation();
